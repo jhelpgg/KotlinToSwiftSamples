2666a63c7cb06569c0716c62519d4dfa9c63e9e3
gerardbourriaud <gerard.bourriaud@feetme.fr>
2020-10-06 17:01:12 +0200

    protocol typealias put in public

--- src/main/swift/Sources/swift/fr/jhelp/kotlinLightSamples/IntToString.swift
+++ src/main/swift/Sources/swift/fr/jhelp/kotlinLightSamples/IntToString.swift
@@ -1,7 +1,7 @@
 
 public class IntToString : Transformer {
-     typealias Transformer_S = Int
-     typealias Transformer_D = String
+     public typealias Transformer_S = Int
+     public typealias Transformer_D = String
 
     public func transform(_ source: Int) -> String
     {
